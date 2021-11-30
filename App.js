b2951f0fd0b346c23bdf0a4700e2444063824248
umairn2368 <umairn2368@gmail.com>
2021-11-30 17:14:01 +0500

    Splash Screen for IOS

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Information from './app/Navigation/information';
+import SplashScreen from 'react-native-splash-screen';
+
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  
   return (
     <NavigationContainer>
       <Information/>
